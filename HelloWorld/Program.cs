2ce02c7237a431828eabc475f6d1de6efd5c64d4
xekk829@gmail.com <esmiraisenbekova08@mail.ru>
2026-09-10 09:28:11 +0300

    Step 7: variables and data types

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,3 +1,65 @@
-﻿Console.WriteLine("Привет от ИСП-241!");
-Console.WriteLine("Автор: Саликов Аскар");
-Console.WriteLine("Год: 2026");
+﻿// using System.Runtime.InteropServices.Marshalling;
+
+// Console.WriteLine("Привет от ИСП-241!");
+// Console.WriteLine("Автор: Саликов Аскар");
+// Console.WriteLine("Год: 2026");
+
+// int age = 20;
+// double gpa = 4.5;
+// bool isStudent = true;
+// string name = "Иван";
+// Console.WriteLine(name);
+// Console.WriteLine(age);
+// Console.WriteLine(gpa);
+// Console.WriteLine(isStudent);
+
+// var city = "Москва";
+// var year = 2026;
+// var pi = 3.14159;
+// var isActive = true;
+
+// string myname = "Аскар";
+// string myGroup = "ИСП-241";
+// int myAge = 17;
+
+// Console.WriteLine($"Меня зовут {myname}, мне {myAge} лет, я учусь в группе {myGroup}.");
+
+// Console.Write("Введите ваше имя: ");
+// string name2 = Console.ReadLine();
+// Console.WriteLine($"Привет, {name2}!");
+
+// Console.Write("Введите ваш возраст: ");
+// string input = Console.ReadLine();
+// int age2 = int.Parse(input);
+// Console.WriteLine($"Через 10 лет тебе будет {age2 + 10} лет.");
+
+// int x = 10;
+// int y = 3;
+// Console.WriteLine(x + y);
+// Console.WriteLine(x - y);
+// Console.WriteLine(x * y);
+// Console.WriteLine(x / y);
+// Console.WriteLine(x % y);
+
+using System.Reflection.PortableExecutable;
+
+string firstName = "Аскар";
+string lastName = "Саликов";
+string group = "ИСП-241";
+int birthYear = 2009;
+double gpa = 4.8;
+bool hasScholarship = true;
+
+int currentYear = 2026;
+int age = currentYear - birthYear;
+
+Console.WriteLine("Студенческое удостоверение");
+Console.WriteLine($"Имя: {firstName} {lastName}");
+Console.WriteLine($"Группа: {group}");
+Console.WriteLine($"Возраст: {age} лет");
+Console.WriteLine($"Средний балл: {gpa}");
+Console.WriteLine($"Стипендия: {hasScholarship}");
+
+Console.Write("\nВведите ваш любимый предмет: ");
+string subject = Console.ReadLine();
+Console.WriteLine($"Отлично! {firstName} любит {subject}.");
